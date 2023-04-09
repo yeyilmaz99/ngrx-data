1b2dc98ca92a00984cadf838669a00fcf802f866
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-04-09 18:28:27 +0300

    ngrx-Data

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PostsListComponent } from './posts/posts-list/posts-list.component';
+import { AddPostComponent } from './posts/add-post/add-post.component';
+import { EditPostComponent } from './posts/edit-post/edit-post.component';
+import { SinglePostComponent } from './posts/single-post/single-post.component';
+import { PostsResolver } from './posts/posts.resolver';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'posts',component:PostsListComponent ,resolve: {posts:PostsResolver}},
+  {path:'posts/add',component:AddPostComponent},
+  {path:'posts/edit/:id',component:EditPostComponent},
+  {path:'posts/post/:id',component:SinglePostComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
