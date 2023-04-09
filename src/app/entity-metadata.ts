878c431baded98e24946ff1fcf3dc598469f6fd2
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-04-09 18:28:17 +0300

    ngrx-Data

---
+++ src/app/entity-metadata.ts
@@ -0,0 +1,9 @@
+import { EntityDataModuleConfig, EntityMetadataMap } from "@ngrx/data";
+
+const entityMetadata:EntityMetadataMap = {
+    Post: {}
+}
+
+export const entityConfig: EntityDataModuleConfig{
+    entityMetadata,
+}
