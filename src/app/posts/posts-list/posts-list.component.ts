44edafae8fc6e9426326f86b17e48c0a62faf61c
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-04-09 18:29:18 +0300

    ngrx-Data

---
+++ src/app/posts/posts-list/posts-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { PostService } from '../post.service';
+import {Observable} from 'rxjs'
+import { Post } from 'src/app/models/post.model';
+
+@Component({
+  selector: 'app-posts-list',
+  templateUrl: './posts-list.component.html',
+  styleUrls: ['./posts-list.component.css']
+})
+export class PostsListComponent implements OnInit {
+
+  posts$:Observable<Post[]>;
+  constructor(private postService:PostService) { }
+
+  ngOnInit(): void {
+    this.posts$ = this.postService.entities$;
+  }
+
+}
